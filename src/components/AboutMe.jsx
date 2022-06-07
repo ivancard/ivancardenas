c6250bcbf7bf698ec6f9bc7b0394be218102b68e
ivancard <ivaanlml@gmail.com>
2022-06-07 01:54:37 -0300

    more about me section done

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,6 +6,14 @@ import bgAboutMe from '../assets/trama.svg';
 import { Modal } from './Modal';
 import { useModal } from '../hooks/useModal';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+
+// pictures for more about me
+import imgMoreAboutMe from '../data/dataMoreAboutMe';
+
 const bgAboutMeStyle = {
     backgroundImage: `url(${bgAboutMe})`,
 };
@@ -17,14 +25,60 @@ export const AboutMe = () => {
         <>
             {modal && (
                 <Modal toggleModal={toggleModal}>
-                    <h1>Hola</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Earum harum animi amet! Facilis maxime fuga quas
-                        laudantium amet mollitia, animi id tempora porro non
-                        unde eum commodi ipsam, odio illo alias eius dignissimos
-                        repellat enim fugiat. Optio aliquam reprehenderit quasi.
-                    </p>
+                    <div className='aboutme-modal-container'>
+                        <h1>Más sobre mi</h1>
+                        <p>
+                            Mi nombre es Ivan Cardenas, nací en Capital Federal,
+                            Buenos Aires, Argentina. Crecí en el partido de
+                            Lomas de Zamora GBA, donde recido actualmente.
+                        </p>
+                        <p>
+                            En la secundaria me nació mi interez por la
+                            tecnología, comencé diseñando infografias y videos
+                            escolares. Por otra parte me interecé en el diseño
+                            3d y tambien conocí HTML, con la cual hice mi
+                            primera web a los 13 años.
+                        </p>
+                        <p>
+                            En los años siguientes me dedique a profundizar en
+                            el mundo informatico y comence a arreglar pcs, eso
+                            me acerco a la programacion al copiar y ejecutar
+                            scripts por terminal.
+                        </p>
+                        <p>
+                            Al mismo tiempo lleve a cabo proyectos de produccion
+                            multimedial. Diseñando branding para marcas, videos,
+                            cubriendo eventos con fotografia, efectos visuales,
+                            diseño 3d, produciendo y dirigiendo cortometrajes,
+                            videos publicitarios y musicales. Manejo muchos
+                            software en esa area, como la suite de Adobe,
+                            daVinci Resolve, Autodesk Maya, Cinema 4D, Nuke,
+                            etc.
+                        </p>
+                        <div className='swipper-container'>
+                            <Swiper
+                                modules={[Autoplay]}
+                                spaceBetween={50}
+                                slidesPerView={1}
+                                autoplay={{ delay: 2000 }}
+                                loop={true}>
+                                {imgMoreAboutMe.map(({ id, img }) => (
+                                    <SwiperSlide key={id}>
+                                        <img src={img} />
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+                        </div>
+                        <p>
+                            Tambien soy musico, toco guitarra, bajo, teclado,
+                            bateria, violin y violoncello. Aprendi por mi cuenta
+                            y enseñe durante un tiempo. Incursione en el mundo
+                            de la produccion musical grabando, mezclando y
+                            masterizando canciones para cantantes en mi home
+                            studio. Estudié lutheria, fabrique y repare varios
+                            instrumentos electricos de cuerda.
+                        </p>
+                    </div>
                 </Modal>
             )}
             <section className='aboutme-container' id='sobreMi'>
@@ -36,9 +90,10 @@ export const AboutMe = () => {
                                 Sobre Mi
                             </h3>
                             <p>
-                                Soy desarrollador de software enfocado al área
-                                web, tanto back-end como front-end, siendo esta
-                                última mi área de mayor especialidad.
+                                Soy desarrollador de software enfocado al
+                                desarrollo web, tanto back-end como front-end,
+                                siendo esta última mi área de mayor
+                                especialidad.
                             </p>
                             <p>
                                 Estudio Ingenieria en informática y realicé
---
+++ src/data/dataMoreAboutMe.js
@@ -0,0 +1,25 @@
+import aboutme1 from '../assets/aboutme-1.jpg';
+import aboutme2 from '../assets/aboutme-2.jpg';
+import aboutme3 from '../assets/aboutme-3.jpg';
+import aboutme4 from '../assets/aboutme-4.jpg';
+import aboutme5 from '../assets/aboutme-5.jpg';
+import aboutme6 from '../assets/aboutme-6.jpg';
+import aboutme7 from '../assets/aboutme-7.jpg';
+import aboutme8 from '../assets/aboutme-8.jpg';
+import aboutme9 from '../assets/aboutme-9.jpg';
+import aboutme10 from '../assets/aboutme-10.jpg';
+import aboutme11 from '../assets/aboutme-11.jpg';
+
+export default [
+    { id: 1, img: aboutme1 },
+    { id: 2, img: aboutme2 },
+    { id: 3, img: aboutme3 },
+    { id: 4, img: aboutme4 },
+    { id: 5, img: aboutme5 },
+    { id: 6, img: aboutme6 },
+    { id: 7, img: aboutme7 },
+    { id: 8, img: aboutme8 },
+    { id: 9, img: aboutme9 },
+    { id: 10, img: aboutme10 },
+    { id: 11, img: aboutme11 },
+];

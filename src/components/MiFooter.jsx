4c650e14ea5dc07ff56d326292abbe839290c537
Ivan Cardenas <ivan.cardenas.35977@hotmail.com>
2023-04-05 13:27:56 -0300

    footer updated

--- src/components/MiFooter.jsx
+++ src/components/MiFooter.jsx
@@ -1,9 +1,9 @@
 import '../scss/footer.scss';
 
 export const MiFooter = () => {
-    return (
-        <footer>
-            <span>Desarrollado por Ivan Cardenas - 2022</span>
-        </footer>
-    );
+  return (
+    <footer>
+      <span>Desarrollado por Ivan Cardenas - 2023</span>
+    </footer>
+  );
 };

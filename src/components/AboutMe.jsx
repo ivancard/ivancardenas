b5dd6ffc536bdb133b46ab8c532987c08dc633c8
Ivan Cardenas <ivan.cardenas.35977@hotmail.com>
2023-04-05 12:49:22 -0300

    aboutme updated

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -15,106 +15,102 @@ import 'swiper/css/autoplay';
 import imgMoreAboutMe from '../data/dataMoreAboutMe';
 
 const bgAboutMeStyle = {
-    backgroundImage: `url(${bgAboutMe})`,
+  backgroundImage: `url(${bgAboutMe})`,
 };
 
 export const AboutMe = () => {
-    const [modal, toggleModal] = useModal();
+  const [modal, toggleModal] = useModal();
 
-    return (
-        <>
-            {modal && (
-                <Modal toggleModal={toggleModal}>
-                    <div className='aboutme-modal-container'>
-                        <h1>Más sobre mi</h1>
-                        <p>
-                            Mi nombre es Ivan Cardenas, nací en Capital Federal,
-                            Buenos Aires, Argentina. Crecí en el partido de
-                            Lomas de Zamora GBA, donde recido actualmente.
-                        </p>
-                        <p>
-                            En la secundaria me nació mi interez por la
-                            tecnología, comencé diseñando infografias y videos
-                            escolares. Por otra parte me interecé en el diseño
-                            3d y tambien conocí HTML, con la cual hice mi
-                            primera web a los 13 años.
-                        </p>
-                        <p>
-                            En los años siguientes me dedique a profundizar en
-                            el mundo informatico y comence a arreglar pcs, eso
-                            me acerco a la programacion al copiar y ejecutar
-                            scripts por terminal.
-                        </p>
-                        <p>
-                            Al mismo tiempo lleve a cabo proyectos de produccion
-                            multimedial. Diseñando branding para marcas, videos,
-                            cubriendo eventos con fotografia, efectos visuales,
-                            diseño 3d, produciendo y dirigiendo cortometrajes,
-                            videos publicitarios y musicales. Manejo muchos
-                            software en esa area, como la suite de Adobe,
-                            daVinci Resolve, Autodesk Maya, Cinema 4D, Nuke,
-                            etc.
-                        </p>
-                        <div className='swipper-container'>
-                            <Swiper
-                                modules={[Autoplay]}
-                                spaceBetween={50}
-                                slidesPerView={1}
-                                autoplay={{ delay: 2000 }}
-                                loop={true}>
-                                {imgMoreAboutMe.map(({ id, img }) => (
-                                    <SwiperSlide key={id}>
-                                        <img src={img} />
-                                    </SwiperSlide>
-                                ))}
-                            </Swiper>
-                        </div>
-                        <p>
-                            Tambien soy musico, toco guitarra, bajo, teclado,
-                            bateria, violin y violoncello. Aprendi por mi cuenta
-                            y enseñe durante un tiempo. Incursione en el mundo
-                            de la produccion musical grabando, mezclando y
-                            masterizando canciones para cantantes en mi home
-                            studio. Estudié lutheria, fabrique y repare varios
-                            instrumentos electricos de cuerda.
-                        </p>
-                    </div>
-                </Modal>
-            )}
-            <section className='aboutme-container' id='sobreMi'>
-                <div className='bg-aboutme' style={bgAboutMeStyle}></div>
-                <div className='skills-description'>
-                    <div className='aboutme-description-container'>
-                        <div className='aboutme-description-text'>
-                            <h3 className='aboutme-description-title'>
-                                Sobre Mi
-                            </h3>
-                            <p>
-                                Soy desarrollador de software enfocado al
-                                desarrollo web, tanto back-end como front-end,
-                                siendo esta última mi área de mayor
-                                especialidad.
-                            </p>
-                            <p>
-                                Estudio Ingenieria en informática y realicé
-                                muchos cursos en diferentes plataformas. Entre
-                                ellas Platzi, Udemy, Crehana, entre otras.
-                            </p>
-                        </div>
-                        <button className='more-aboutme' onClick={toggleModal}>
-                            Más sobre mi
-                        </button>
-                    </div>
-                    <div className='aboutme-skills-container'>
-                        <h3>Skills</h3>
-                        <div className='aboutme-skills-list'>
-                            {skills.map(({ id, title, img }) => (
-                                <Skill key={id} title={title} img={img} />
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      {modal && (
+        <Modal toggleModal={toggleModal}>
+          <div className='aboutme-modal-container'>
+            <h1>Más sobre mi</h1>
+            <p>
+              Mi nombre es Ivan Cardenas, nací en Capital Federal, Buenos Aires,
+              Argentina. Crecí en el partido de Lomas de Zamora GBA, donde
+              recido actualmente.
+            </p>
+            <p>
+              En la secundaria me nació mi interez por la tecnología, comencé
+              diseñando infografias y videos escolares. Por otra parte me
+              interecé en el diseño 3d y tambien conocí HTML, con la cual hice
+              mi primera web a los 13 años.
+            </p>
+            <p>
+              En los años siguientes me dedique a profundizar en el mundo
+              informatico y comence a arreglar pcs, eso me acerco a la
+              programacion al copiar y ejecutar scripts por terminal.
+            </p>
+            <p>
+              Al mismo tiempo lleve a cabo proyectos de produccion multimedial.
+              Diseñando branding para marcas, videos, cubriendo eventos con
+              fotografia, efectos visuales, diseño 3d, produciendo y dirigiendo
+              cortometrajes, videos publicitarios y musicales. Manejo muchos
+              software en esa area, como la suite de Adobe, daVinci Resolve,
+              Autodesk Maya, Cinema 4D, Nuke, etc.
+            </p>
+            <div className='swipper-container'>
+              <Swiper
+                modules={[Autoplay]}
+                spaceBetween={50}
+                slidesPerView={1}
+                autoplay={{ delay: 2000 }}
+                loop={true}
+              >
+                {imgMoreAboutMe.map(({ id, img }) => (
+                  <SwiperSlide key={id}>
+                    <img src={img} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+            <p>
+              Tambien soy musico, toco guitarra, bajo, teclado, bateria, violin
+              y violoncello. Aprendi por mi cuenta y enseñe durante un tiempo.
+              Incursione en el mundo de la produccion musical grabando,
+              mezclando y masterizando canciones para cantantes en mi home
+              studio. Estudié lutheria, fabrique y repare varios instrumentos
+              electricos de cuerda.
+            </p>
+          </div>
+        </Modal>
+      )}
+      <section className='aboutme-container' id='sobreMi'>
+        <div className='bg-aboutme' style={bgAboutMeStyle}></div>
+        <div className='skills-description'>
+          <div className='aboutme-description-container'>
+            <div className='aboutme-description-text'>
+              <h3 className='aboutme-description-title'>Sobre Mi</h3>
+              <p className='first-paragraph'>
+                Soy un desarrollador iOS que utiliza las tecnologías nativas de
+                iOS para su desarrollo. Me destaco por mi capacidad de prestar
+                atención a los detalles y asegurarme de que cada aspecto de mi
+                trabajo este bien ejecutado.
+              </p>
+              <p>
+                Me motiva buscar siempre soluciones innovadoras a los problemas
+                que se presentan y procurar colaborar con mi equipo para lograr
+                objetivos comunes y alcanzar los mejores resultados posibles.
+                Tambien tengo conociemientos en desarrollo web con react y
+                backend con node, vapor y java.
+              </p>
+            </div>
+            <button className='more-aboutme' onClick={toggleModal}>
+              Más sobre mi
+            </button>
+          </div>
+          <div className='aboutme-skills-container'>
+            <h3>Skills</h3>
+            <div className='aboutme-skills-list'>
+              {skills.map(({ id, title, img }) => (
+                <Skill key={id} title={title} img={img} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };

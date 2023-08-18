5848d066c8b6eb93221163718edf395eb3a191cf
Ivan <78120099+ivancard@users.noreply.github.com>
2023-08-17 23:51:56 -0300

    Update HeaderNav.jsx

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import '../scss/header-nav.scss';
 import { Window } from './Window';
 
-import fotoPerfil from '../assets/foto-perfilg.png';
+import fotoPerfil from '../assets/foto-perfil.png';
 
 const Path = (props) => (
   <motion.path
